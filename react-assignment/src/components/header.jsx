7e4ba12b3a5d65a24c651077088be563e6da316d
muthembw <clairebridget6@gmail.com>
2025-03-07 19:16:44 +0300

    Added mern stack

---
+++ react-assignment/src/components/header.jsx
@@ -0,0 +1,10 @@
+const Header = ({ title }) => {
+    return (
+      <header className="bg-blue-500 text-white p-4 text-center text-xl font-bold">
+        {title}
+      </header>
+    );
+  };
+  
+  export default Header;
+  
